d2eb284143aa49f3621d168aadca21fa0a0b0ba9
aldairribeiro <aldair.martis0@gmail.com>
2023-08-30 10:15:26 -0300

    ADD:variables

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,2 @@
-do_token="colocar o token"
-droplet_name="vm-modulo-terraform"
-droplet_region="nyc3"
-#droplet_size=
-#ssh_key_name=
+do_token= "colocar o token"
+vms_count= 1
--- variables.tf
+++ variables.tf
@@ -1,19 +1,34 @@
 variable "do_token" {
-
+  type = string
+  description = "token da API da Digital Ocean"
 }
 
 variable "droplet_name" {
   default = "vm-aula"
+  type = string
+  description = "nome inicial do droplet"
 }
 
 variable "droplet_region" {
   default = "nyc1"
+  type = string
+  description = "Região que vai ser craida a infra"
 }
 
 variable "droplet_size" {
   default = "s-1vcpu-1gb"
+  type = string
+  description = "Perfil de maquina dos droplets"
 }
 
 variable "ssh_key_name" {
   default = "aula-terraform"
+  type = string
+  description = "chave ssh que vai ser utilizado"
+}
+
+variable "vms_count" {
+  default = 1
+  type = number
+  description = "quantidade de maquinas"
 }
